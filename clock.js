3331ba290c67848deaa6248520deff118a567b6d
Timoty Soren <timtoysoren03@gmail.com>
2024-04-22 13:57:53 +0530

    intial commit

---
+++ clock.js
@@ -0,0 +1,15 @@
+/* Javascript code for Clock */
+
+// Targetting all the elements
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+//updating the time 
+setInterval(()=>{
+  let currentTime = new Date();
+
+  hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+  min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+  sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+},1000)
